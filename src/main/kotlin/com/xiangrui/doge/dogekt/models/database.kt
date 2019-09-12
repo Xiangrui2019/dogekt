2f8077f7927bb0e4c623256dce14c345dec75a2c
Go Go Go <xiangrui@aiursoft.com>
2019-09-12 22:02:08 +0800

    connect

--- src/main/kotlin/com/xiangrui/doge/dogekt/models/database.kt
+++ src/main/kotlin/com/xiangrui/doge/dogekt/models/database.kt
@@ -1,7 +1,11 @@
 package com.xiangrui.doge.dogekt.models
 
+import com.xiangrui.doge.dogekt.conf.env
 import org.jetbrains.exposed.sql.Database
 
 fun ConnectDatabase() {
+    Database.connect(env?.get("MYSQL_DSN")!!, "",
+        env?.get("MYSQL_USER")!!, env?.get("MYSQL_PASS")!!)
+
     MigrationModels()
 }
